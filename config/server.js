e67882565b42399995810034701daf25615fc975
LeTea <iamletea@gmail.com>
2021-01-30 10:31:21 +0800

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e47059145015b64ad4356fdc1bbd5d8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b899513-ef39-4bdc-a4f2-a0807656d8ad'
+};
